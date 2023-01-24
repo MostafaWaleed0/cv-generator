8c3990516793875e9c8fd8e4e8993dd93a3893bf
Mostafa Waleeed <mostafawaleed206@gmail.com>
2023-01-24 13:24:42 +0200

    ✨ feat: improve SEO

--- components/common/Container/Container.tsx
+++ components/common/Container/Container.tsx
@@ -10,9 +10,9 @@ export default function Container(props: {
   const { children, ...customMeta } = props;
 
   const meta = {
-    title: '',
-    description: ``,
-    image: '',
+    title: 'Free CV Builder Make Your CV Online Quickly - MW',
+    description: `It might be difficult to get started when you're doing it all by yourself while creating a curriculum vitae. Let's do better than the competitors and land you the ideal position.`,
+    image: 'https://cv-generator-mw.vercel.app/static/favicons/android-chrome-512x512.png',
     ...customMeta
   };
 
@@ -21,8 +21,11 @@ export default function Container(props: {
       <Head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <title>{meta.title}</title>
-        <link rel="canonical" href={`https://www..${router.asPath}`} />
+        <meta content={meta.description} name="description" />
+        <meta name="robots" content="follow, index" />
+        <link rel="canonical" href={`https://cv-generator-mw.vercel.app${router.asPath}`} />
       </Head>
       <Header />
       {children}
