4ce8a30762a77623a6f99b89ed9c362af11a2f45
Mostafa Waleeed <mostafawaleed206@gmail.com>
2023-01-24 13:23:55 +0200

    ✨ feat: add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,21 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/static/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta content="/static/favicons/browserconfig.xml" name="msapplication-config" />
+        <meta
+          content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+          name="robots"
+        />
+      </Head>
       <body className="pixel-background">
         <Main />
         <NextScript />
