96a34ddfe8c7c315beac23778425378eb09ada6a
Mostafa Waleeed <mostafawaleed206@proton.me>
2023-02-14 10:42:59 +0200

    Add comments to small functions in utilities

--- utils/check-errors.ts
+++ utils/check-errors.ts
@@ -1,5 +1,12 @@
 import { ErrorsType } from 'lib/types';
 
+/**
+ * Checks if an object of errors is not empty for a given key.
+ *
+ * @param {ErrorsType} errors - The object containing errors to check.
+ * @param {keyof ErrorsType} key - The key of the errors object to check.
+ * @returns {boolean} Returns true if the errors object is not empty, false otherwise.
+ */
 export const checkErrors = (errors: ErrorsType, key: keyof ErrorsType): boolean => {
   return errors && Object.keys(errors[key]).length > 0;
 };
